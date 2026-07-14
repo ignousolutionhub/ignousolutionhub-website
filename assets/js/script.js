58cd1088265cbd492f47a56204f09fdd3aed6ead
ignousolutionhub <ignouprojectpractical@gmail.com>
2026-07-14 23:50:07 +0530

    Implement course search functionality
    Added a function to search for courses and redirect.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
+function searchCourse(){
 
+let course=document.getElementById("searchInput").value.toUpperCase();
+
+if(course=="BCA"){
+window.location="courses.html";
+}
+else if(course=="MCA"){
+window.location="courses.html";
+}
+else if(course=="BAG"){
+window.location="courses.html";
+}
+else if(course=="BLIS"){
+window.location="courses.html";
+}
+else{
+alert("Course not found.");
+}
+
+}
