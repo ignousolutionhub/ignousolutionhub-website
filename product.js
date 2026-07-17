dc0cce08f842346b6fcd8440c51b35ad7a91c336
ignousolutionhub <ignouprojectpractical@gmail.com>
2026-07-17 22:29:53 +0530

    Implement product form submission with Firestore
    Added product submission functionality to form.

--- product.js
+++ product.js
@@ -1,1 +1,36 @@
+import { db } from "./firebase-config.js";
+import {
+  collection,
+  addDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
+const form = document.getElementById("productForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const product = {
+    type: document.getElementById("productType").value,
+    course: document.getElementById("course").value,
+    subject: document.getElementById("subject").value,
+    title: document.getElementById("title").value,
+    price: document.getElementById("price").value,
+    description: document.getElementById("description").value,
+    createdAt: serverTimestamp()
+  };
+
+  try {
+    await addDoc(collection(db, "products"), product);
+
+    alert("Product Published Successfully");
+
+    form.reset();
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+});
