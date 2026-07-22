09eef80f4a72194934c5002311e0858619b20061
ignousolutionhub <ignouprojectpractical@gmail.com>
2026-07-22 22:08:39 +0530

    Update dashboard.js

--- admin/dashboard.js
+++ admin/dashboard.js
@@ -1,22 +1,24 @@
 import { auth } from "../firebase-config.js";
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
-// Check if admin is logged in
 onAuthStateChanged(auth, (user) => {
     if (!user) {
         window.location.href = "../admin.html";
     }
 });
 
-// Logout
-document.getElementById("logoutBtn").addEventListener("click", async (e) => {
-    e.preventDefault();
+const logoutBtn = document.getElementById("logoutBtn");
 
-    try {
-        await signOut(auth);
-        alert("Logged out successfully.");
-        window.location.href = "../admin.html";
-    } catch (error) {
-        alert(error.message);
-    }
-});
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        try {
+            await signOut(auth);
+            alert("Logged out successfully.");
+            window.location.href = "../admin.html";
+        } catch (error) {
+            alert(error.message);
+        }
+    });
+}
