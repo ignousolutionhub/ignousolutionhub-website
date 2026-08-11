f6d966071537154f157e1363948e5fbc506839a5
ignousolutionhub <ignouprojectpractical@gmail.com>
2026-08-12 01:13:15 +0530

    Enable EmailJS automatic customer order emails

--- email-config.js
+++ email-config.js
@@ -1,9 +1,5 @@
 // EmailJS configuration for automatic customer order emails.
-// Create a FREE EmailJS account and put your values below.
-// The email will be sent from payment.html after the order is saved.
-export const EMAILJS_PUBLIC_KEY = "YOUR_EMAILJS_PUBLIC_KEY";
-export const EMAILJS_SERVICE_ID = "YOUR_EMAILJS_SERVICE_ID";
-export const EMAILJS_TEMPLATE_ID = "YOUR_EMAILJS_TEMPLATE_ID";
-
-// Keep true after entering all 3 values above.
-export const EMAILJS_ENABLED = false;
+export const EMAILJS_PUBLIC_KEY = "7aciplkjhZUwmuwv8";
+export const EMAILJS_SERVICE_ID = "service_bl3gsj8";
+export const EMAILJS_TEMPLATE_ID = "template_ygrbyh3";
+export const EMAILJS_ENABLED = true;
